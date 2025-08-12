565f601a76b4cc85e10f02c8d07fb35ed7cbe708
Ritujakundalwal <141045113+Ritujakundalwal@users.noreply.github.com>
2025-08-12 17:32:59 +0530

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class HelpRequest(models.Model):
-    seeker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='helprequests')  # Related name वापरल्यामुळे user.helprequests.all() मिळेल
+    seeker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='helprequests')  
     help_type = models.CharField(max_length=255)
     description = models.TextField()
     area = models.CharField(max_length=100)
     is_accepted = models.BooleanField(default=False)
-    accepted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='accepted_helpers')  # Helper साठी related_name
+    accepted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='accepted_helpers')  
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
